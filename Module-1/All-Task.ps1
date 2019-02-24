e73d84aaff5a0e25384042aaa6367410d76e1f75
Viktar Tsybulko <Death_Knight@tut.by>
2019-02-24 16:18:10 +0300

    Check

---
+++ Module-1/All-Task.ps1
@@ -0,0 +1,17 @@
+# Подключиться к обоим серверам MSSQL
+
+# локальное подключение (идентификация win)
+Import-Module SqlServer
+$myLogin = "sa"
+$myPass = Get-Credential -UserName $myLogin -Message "Enter Password"
+Invoke-Sqlcmd -ServerInstance $env:COMPUTERNAME -Credential $myPass -Query "select @@SERVERNAME;"
+
+
+# подключение к удаленному серверу
+$myTempPass = Read-Host "Enter password for User 'sa'?" -AsSecureString
+$myPass1 = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($myTempPass))
+ Invoke-Sqlcmd -ServerInstance 192.168.130.1  -Username "sa" -Password $myPass1 -Query "select @@servername;"
+ Invoke-Sqlcmd -ServerInstance 192.168.130.2  -Username "sa" -Password $myPass1 -Query "select @@servername;"
+ # Invoke-Sqlcmd -ServerInstance 192.168.130.10  -Username "sa" -Password $myPass1 -Query "select @@servername;"
+
+ 
