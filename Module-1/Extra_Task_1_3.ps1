275c7fa1492c02a08d8d0b2fa5bdde7ca82aff53
Viktar Tsybulko <Death_Knight@tut.by>
2019-02-27 14:02:41 +0300

    Check point

--- Module-1/Extra_Task_1_3.ps1
+++ Module-1/Extra_Task_1_3.ps1
@@ -1,4 +1,4 @@
-# автоматическая уставновка 
+# автоматическая уставновка. local version
 $SQLSetupSource = "E:\setup.exe"
 $ConfFileSource = "C:\Install\def.ini" # default instance
 & $SQLSetupSource /QS /ConfigurationFile=$ConfFileSource /IACCEPTSQLSERVERLICENSETERMS
@@ -6,18 +6,6 @@ $ConfFileSource = "C:\Install\def.ini" # default instance
 $ConfFileSource1 = "C:\Install\def-named.ini" #named instance
 & $SQLSetupSource /QS /ConfigurationFile=$ConfFileSource1 /IACCEPTSQLSERVERLICENSETERMS
 
-
-
-# создаем правила вфаерволе для возможности подключения
-# Import-Module NetSecurity
-New-NetFirewallRule -DisplayName "--- Inbound MSSQL TCP 1433" -Direction "Inbound" `
-    -Action "Allow" -Profile "Any" -Protocol "TCP" -LocalPort "1433" 
-
-New-NetFirewallRule -DisplayName "--- Inbound MSSQL UDP 1434" -Direction "Inbound" `
-    -Action "Allow" -Profile "Any" -Protocol "UDP" -LocalPort "1434" 
-
-New-NetFirewallRule -DisplayName "--- Inbound RDP TCP 3389" -Direction "Inbound" `
-    -Action "Allow" -Profile "Any" -Protocol "TCP" -LocalPort "3389" 
 
 # посмотрим имена машин и развернутых на них инстансах
 [System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources()
@@ -32,20 +20,74 @@ New-NetFirewallRule -DisplayName "--- Inbound RDP TCP 3389" -Direction "Inbound"
 # Where-Object {$_.Name -match "SQL" -AND $_.vendor -eq "Microsoft Corporation"} | `
 # Select-Object Name, Version
 
+# -------------------------------------
+# Settings
+# -------------------------------------
+
+$vms = "192.168.130.1", "192.168.130.2"
+$vm1 = "192.168.130.1"
+$vm2 = "192.168.130.2"
+
+$myLogin = Get-Credential -UserName Administrator -Message "Admin login to VM."
+
+$FWRule1 = "---1 Inbound RDP TCP 3389"
+$FWRule2 = "---1 Inbound MSSQL TCP 1433"
+$FWRule3 = "---1 Inbound MSSQL UDP 1434"
+
+$InstallNamedInstance = $true
+$NameOfNamedInstance = "DEVOPS2019"
+$FWRule4 = "---1 SQL Named Instance $NameOfNamedInstance"
+$SQLver = "12"
+$p_path = "%ProgramFiles%\Microsoft SQL Server\MSSQL$SQLver.$NameOfNamedInstance\MSSQL\Binn\sqlservr.exe"
+
+$mySQLLogin = "sa"
+$newTempDBdir = "D:\new_tempdb"
+
+$def_data_path = "C:\Program Files\Microsoft SQL Server\MSSQL$SQLver.MSSQLSERVER\MSSQL\DATA"
+# tempdb.mdf and tempdb.ldf
+
+# -------------------------------------
+
+# add firewall rules + retun their states
+# Import-Module NetSecurity
+
+$FWRulesScriptStandart = {   
+    $FWrules = @($Using:FWRule1, $Using:FWRule2, $Using:FWRule3)       
+
+    New-NetFirewallRule -DisplayName $Using:FWRule1 -Direction "Inbound" `
+        -Action "Allow" -Profile "Any" -Protocol "TCP" -LocalPort "3389" 
+    New-NetFirewallRule -DisplayName $Using:FWRule2 -Direction "Inbound" `
+        -Action "Allow" -Profile "Any" -Protocol "TCP" -LocalPort "1433" 
+    New-NetFirewallRule -DisplayName $Using:FWRule3 -Direction "Inbound" `
+        -Action "Allow" -Profile "Any" -Protocol "UDP" -LocalPort "1434"         
+    
+    if ($Using:InstallNamedInstance -eq $true) {
+        # $FWrules = @($Using:FWRule1, $Using:FWRule2, $Using:FWRule3, $Using:FWRule4)
+        $FWrules += $Using:FWRule4
+        New-NetFirewallRule -DisplayName $Using:FWRule4 -Direction "Inbound" `
+            -Action "Allow" -Profile "Any" -Program $Using:p_path
+    }    
+    foreach ($rule in $FWrules) {
+        Get-NetFirewallRule -DisplayName $rule | `
+            Select-Object PSComputerName, DisplayName, Enabled | Format-Table
+    }
+}
+Invoke-Command -ComputerName $vm1 -ScriptBlock $FWRulesScriptStandart -Credential $myLogin
+
+
 # посмотр установленных фич для серверов SQL
-$computers = "192.168.130.1","192.168.130.1","192.168.130.1","admin"
+
 $results = @()
-foreach ($computer in $computers) {
+foreach ($vm in $vms) {
     Try {
-        $Reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $computer)
-        $RegKey= $Reg.OpenSubKey("SOFTWARE\\Microsoft\\Microsoft SQL Server\\Instance Names")
+        $Reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $vm)
+        $RegKey = $Reg.OpenSubKey("SOFTWARE\\Microsoft\\Microsoft SQL Server\\Instance Names")
         $features = $RegKey.GetSubKeyNames();
 
-        foreach ($i in $features) 
-        {
+        foreach ($i in $features) {
             ## Get installed Features 
             $key = "SOFTWARE\\Microsoft\\Microsoft SQL Server\\Instance Names\\$i"
-            $FeatureKey= $Reg.OpenSubKey($key)
+            $FeatureKey = $Reg.OpenSubKey($key)
 
             $Value = $FeatureKey.GetValueNames();
 
@@ -54,10 +96,10 @@ foreach ($computer in $computers) {
                 if ($instance -ne '') {
                     $instanceName = $FeatureKey.GetValue($instance)
                     $key = "SOFTWARE\\Microsoft\\Microsoft SQL Server\\$instanceName\\Setup"
-                    $instanceKey= $Reg.OpenSubKey($key)
+                    $instanceKey = $Reg.OpenSubKey($key)
 
                     $object = New-Object PSObject
-                    Add-Member -InputObject $object -MemberType NoteProperty -Name Computer -Value $computer
+                    Add-Member -InputObject $object -MemberType NoteProperty -Name Computer -Value $vm
                     Add-Member -InputObject $object -MemberType NoteProperty -Name Feature -Value $i
                     Add-Member -InputObject $object -MemberType NoteProperty -Name Version -Value $instanceKey.GetValue('Version')
                     Add-Member -InputObject $object -MemberType NoteProperty -Name Edition -Value $instanceKey.GetValue('Edition')
@@ -67,8 +109,120 @@ foreach ($computer in $computers) {
         }
     }
     Catch {
-        Write-Host "$computer Not Reachable"
+        Write-Host "$vm - host is unreachable."
     }
 }
 # $results | Out-GridView
 $results
+
+
+# ----------------------------------------
+# move tempdb
+# ----------------------------------------
+
+# create new folfer
+
+$prepareFolder = {    
+    if (!(Test-Path -Path $Using:newTempDBdir)) {
+        Write-Output "no folder. creating..."
+        $f = New-Item -Path $Using:newTempDBdir -ItemType Directory
+        Write-Output "done"        
+    }
+    else {
+        Write-Output "folder exist"
+        Get-ChildItem -Path $Using:newTempDBdir
+    }
+}
+Invoke-Command -ComputerName $vm1 -ScriptBlock $prepareFolder -Credential $myLogin
+
+# execute moving sql-code
+$mySQLLogin = "sa"
+$mySQLPass = Get-Credential -UserName $mySQLLogin -Message "SQL 'sa' password."
+# filename = N"'$Using:newTempDBdir\tempdb.mdf'")
+# $ff1 = "N'$newTempDBdir\tempdb.mdf'"
+$movingdb_SQLquery = {
+    use master
+    alter database tempdb
+    modify file(
+    name = tempdev,    
+    filename = N'D:\new_tempdb\tempdb.mdf')
+    go
+
+    alter database tempdb
+    modify file(
+    name = templog,
+    filename = N'D:\new_tempdb\templog.ldf')
+    go
+}
+# $newTempDBdir
+# $s_temp = "use master alter database tempdb modify file(name = tempdev, filename = N'D:\"+$newTempDBdir+"\tempdb.mdf') go"
+# $s_temp = "use master\n alter database tempdb\n modify file(\nname = tempdev, \nfilename = N'D:\"+$newTempDBdir+"\tempdb.mdf') \ngo"
+Invoke-Sqlcmd -ServerInstance $vm1 -Credential $mySQLPass -Query $movingdb_SQLquery
+
+
+# restart  SQL service
+
+$RestartSQLserviseScript = {
+    $s = Get-Service -ServiceName "MSSQLSERVER"
+    Write-Output ("Service "+$s.ServiceName+" status: "+$s.Status+". Waiting...")
+    $s.Stop()
+    Start-Sleep -Seconds 20
+    $s.Refresh()
+    Write-Output ("Service "+$s.ServiceName+" status: "+$s.Status+". Waiting...")    
+    $s.Start()
+    Start-Sleep -Seconds 20
+    $s.Refresh()
+    Write-Output ("Service "+$s.ServiceName+" status: "+$s.Status+". Done.")    
+    # check new files in new directory
+    Get-ChildItem -Path $Using:newTempDBdir
+}
+Invoke-Command -ComputerName $vm1 -ScriptBlock $RestartSQLserviseScript -Credential $myLogin
+
+
+# remove old tempdb.mdf and templog.ldf
+
+$RemoveOldFiles = {
+    try {
+        Remove-Item -Path "$Using:def_data_path\tempdb.mdf"
+        Remove-Item -Path "$Using:def_data_path\templog.ldf"
+        Get-ChildItem -Path $Using:def_data_path
+        Write-Output ("tempdb.mdf, templog.ldf - its gone.")
+    }
+    catch {
+        Write-Output ("Warning! can't delete 'tempdb.mdf' or 'templog.ldf'")        
+    }    
+}
+Invoke-Command -ComputerName $vm1 -ScriptBlock $RemoveOldFiles -Credential $myLogin
+
+# -------------------------------------------
+
+$da
+$qw = "CREATE DATABASE Sales ON (NAME = Sales_dat,    FILENAME = 'D:\Data\Sales.mdf',
+ SIZE = 100MB, MAXSIZE = 500MB, FILEGROWTH = 20%) LOG ON   (NAME = Sales_log,
+    FILENAME = 'D:\Logs\Sales.ldf', SIZE = 20MB, MAXSIZE = UNLIMITED, FILEGROWTH = 10MB); "
+Invoke-Sqlcmd -ServerInstance $vm1 -Credential $mySQLPass -Query $qw
+
+$qw_drop = "DROP DATABASE Sales"
+Invoke-Sqlcmd -ServerInstance $vm1 -Credential $mySQLPass -Query $qw_drop
+
+
+
+$nw = "
+    use master
+    alter database tempdb
+    modify file(
+    name = tempdev,    
+    filename = N'$newTempDBdir\tempdb.mdf')
+    go
+
+    alter database tempdb
+    modify file(
+    name = templog,
+    filename = N'$newTempDBdir\templog.ldf')
+    go
+"
+Invoke-Sqlcmd -ServerInstance $vm1 -Credential $mySQLPass -Query $nw
+
+$movingdb_SQLquery = {
+    
+}
--- test.ps1
+++ test.ps1
@@ -10,8 +10,8 @@ get-help Get-SqlInstance  -Examples
 Get-Service *SQL*
 
 Get-WmiObject Win32_product | `
-Where-Object {$_.Name -match "SQL" -AND $_.vendor -eq "Microsoft Corporation"} | `
-Select-Object Name, Version
+    Where-Object {$_.Name -match "SQL" -AND $_.vendor -eq "Microsoft Corporation"} | `
+    Select-Object Name, Version
 
 Get-NetFirewallRule | Select-Object -First 3
 $r = Get-NetFirewallRule -name vm-monitoring-icmpv4
@@ -19,3 +19,134 @@ $r.GetCimSessionComputerName()
 
 Get-NetFirewallProfile -PolicyStore ActiveStore
 
+# $myPassport = Get-Credential Administrator
+# $vms = "192.168.130.1","192.168.130.2"
+$myPassport = Get-Credential 'Server2\Администратор'
+$vms = "192.168.100.242"
+$myScript = {    
+    $vmn = (Get-WmiObject Win32_OperatingSystem).CSName
+    $Sers = "ALG" #stop
+    #$Sers = "BFE" #run
+    $se = Get-Service $Sers
+    if ($se.Status -eq "Running") {    
+        Write-Host -f "green" $vmn, $se.Name -Separator "-"
+    }
+    else {
+        Write-Host -f "red"  $vmn, $se.Name -Separator "-"
+    }
+}
+foreach ($vm in $vms) {
+    Invoke-Command -ComputerName $vm -ScriptBlock $myScript -Credential $myPassport
+}
+
+$myPassport1 = Get-Credential VM1\Administrator
+$myScript1 = {
+    # "& E:\setup.exe"
+    # start-process -filepath "& E:\setup.exe" -argumentlist ""
+    # start-process -filepath "E:\setup.exe"
+    #  start-process -filepath C:\TC\totalcmd.exe
+    # ([wmiclass]"\\192.168.130.1\root\cimv2:Win32_Process").Create('C:\TC\totalcmd.exe')
+    # cmd /c 'C:\TC\totalcmd.exe'
+    # ([wmiclass]"Win32_Process").Create('C:\TC\totalcmd.exe')
+    # if (Test-Path -Path "C:\TC\totalcmd.exe") {
+    #     Write-Output "YES"
+    #     $t = Get-WmiObject Win32_Process
+    #     $t.Create("C:\TC\totalcmd.exe")
+    # }
+    # calc.exe
+    PING.EXE "tut.by"
+}
+# Invoke-Command -ComputerName "192.168.130.1" -ScriptBlock $myScript1 -Credential $myPassport1
+Invoke-Command -ComputerName "192.168.130.1" -ScriptBlock $myScript1 -Credential $myPassport
+
+
+$s = New-PSSession -computername "192.168.130.1" -credential $myPassport1
+Invoke-Command -session $s -scriptblock { & 'C:\TC\totalcmd.exe' }
+Remove-PSSession $s
+
+$t = Get-WmiObject Win32_Process
+$t.Create("c:\Total Commander\totalcmd.exe")
+
+winrs -r:"192.168.130.1" "C:\TC\totalcmd.exe"
+
+$Computer = "vm1"
+$Command = "C:\TC\totalcmd.exe" 
+([wmiclass]"\\$Computer\root\cimv2:Win32_Process").create($Command)
+
+wmic /node:"localhost" process call create "c:\Total Commander\totalcmd.exe" # !
+wmic /node:"192.168.130.1" process call create "C:\TC\totalcmd.exe"
+
+Test-WSMan "192.168.130.1"
+Test-WSMan "192.168.100.242"
+
+Invoke-Command -ComputerName "192.168.130.1" -ScriptBlock { 'C:\TC\totalcmd.exe' } -Credential $myPassport
+
+get-item wsman:\localhost\Client\TrustedHosts
+
+Invoke-Wmimethod -ComputerName "192.168.130.1" -Path win32_process -Name create -Argumentlist calc.exe
+Invoke-Wmimethod -ComputerName "192.168.100.242" -Path win32_process -Name create -Argumentlist calc.exe
+
+PING.EXE "tut.by"
+
+
+# add firewall rules
+
+$InstallNamedInstance = $true
+$NameOfNamedInstance = "DEVOPS2019"        
+if ($InstallNamedInstance) {
+    $FWRule4 = "---1 SQL Named Instance $InstallNamedInstance"
+    $SQLver = "12"
+    $p_path = "%ProgramFiles%\Microsoft Sql Server\MSSQL$SQLver.$NameOfNamedInstance\MSSQL\Binn\sqlservr.exe"
+    # $p_path
+    New-NetFirewallRule -DisplayName "--FF" -Direction "Inbound" -Action "Allow" -Profile "Any" -Program $p_path
+}
+
+Get-NetFirewallRule -DisplayName "--- Inbound RDP TCP 3389" `
+    | Select-Object PSComputerName, DisplayName, Enabled | Format-Table
+
+
+Get-Service *SQL*
+
+$newTempDBdir = "D:\new_tempdb"
+if (!(Test-Path -Path $newTempDBdir)) {
+    Write-Output "no folder"
+    # New-Item -Path $newTempDBdir -ItemType Directory
+}
+
+$s = "'text"
+$s
+
+Get-Service -ServiceName Spooler
+$s = Get-Service -ServiceName Spooler
+$s.Status
+$s.Stop()
+Start-Sleep -Seconds 20
+$s.Refresh()
+$s.Status
+$s.Start()
+Start-Sleep -Seconds 20
+$s.Refresh()
+$s.Status
+
+
+$s = "'$newTempDBdir\tempdb.ldf'"
+$s
+
+get-childitem -path "D:\111"
+
+$MWFO_Log = "Microsoft-Windows-Forwarding/Operational"
+Invoke-Command -ComputerName Server01 -ScriptBlock {Get-EventLog -LogName $Using:MWFO_Log -Newest 10}
+
+$s = "1000"
+"'$s"
+
+
+# Чтобы получить путь скрипта:
+$script_path = $MyInvocation.MyCommand.Path | split-path -parent
+$script_path
+# Чтобы получить имя файла скрипта:
+$script_name = $MyInvocation.MyCommand.Name
+$script_name
+
+"PSScriptRoot is $PSScriptRoot"
+"PSCommandPath is $PSCommandPath"
